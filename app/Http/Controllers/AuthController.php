16d17fb53f2f6596ed3d419480b756af35b4b49d
mianazeemdaula <mianazeemdaula@gmail.com>
2023-11-21 18:31:58 +0500

    init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+
+class AuthController extends Controller
+{
+    //
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('facebook')->redirect();
+});
+ 
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('facebook')->user();
+ 
+    return $user->token;
+});
